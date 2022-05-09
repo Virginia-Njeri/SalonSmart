1db8b65d4da1d2dbe534d42365b40bd5e3c05113
virginia-njeri <virginiaanjeri@gmail.com>
2022-05-09 23:23:26 +0300

    first commit

---
+++ app/src/main/java/com/example/salonsmart/loginActvity.kt
@@ -0,0 +1,11 @@
+package com.example.salonsmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_actvity)
+    }
+}
